5a1b13cd3717ffd6a78750703a3521ea97ee8a9c
indiodev <jhollyfer.fr@gmail.com>
2024-12-26 06:51:35 -0300

    feat: loading

--- assests/main.js
+++ assests/main.js
@@ -16,3 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // changeBackground();
 });
+
+const bar_progress = document.getElementById("bar-progress");
+const loading = document.getElementById("loading");
+
+let progress = 0;
+
+const simulateProgress = setInterval(function () {
+  progress += 1;
+
+  if (progress <= 100) bar_progress.style.width = `${progress}%`;
+
+  if (progress === 100) {
+    progress = 0;
+  }
+}, 50);
+
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    clearInterval(simulateProgress);
+    loading.classList.add("hidden");
+  }, 2000);
+});
