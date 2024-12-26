378a6b99d20dbd5871f693e93eae00a989309884
indiodev <jhollyfer.fr@gmail.com>
2024-12-26 06:13:38 -0300

    fix: rebase and merged

--- assests/main.js
+++ assests/main.js
@@ -1,1 +1,18 @@
 lucide.createIcons();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.getElementById("header");
+  const home = document.getElementById("home");
+
+  const changeBackground = function () {
+    const bounds = home.getBoundingClientRect();
+
+    if (!(bounds.top <= 0 && bounds.bottom > 0))
+      header.classList.add("bg-[#1E1E1E]");
+    else header.classList.remove("bg-[#1E1E1E]");
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
+  // changeBackground();
+});
