10f3bd7017adc6848dd87b62cf1681aa64bf326b
indiodev <jhollyfer.fr@gmail.com>
2025-01-09 12:42:52 -0500

    update info

--- assests/main.js
+++ assests/main.js
@@ -7,33 +7,6 @@ const sr = ScrollReveal({
   reset: true,
 });
 
-function populateGallery() {
-  const MAX_GALLERY = 47;
-  const gallery = document.getElementById("horizontal-gallery");
-
-  const GALLERY_ITEMS = Array.from({ length: MAX_GALLERY }).map((_, index) => {
-    const name = index + 1;
-    const src = "/gallery/".concat(name).concat(".jpeg");
-    const alt = "Imagem ".concat(name);
-    return {
-      name,
-      src,
-      alt,
-    };
-  });
-
-  for (let item of GALLERY_ITEMS) {
-    const li = document.createElement("li");
-    const img = document.createElement("img");
-    li.className = "w-48 h-32 bg-gray-300 flex-shrink-0";
-    img.src = item.src;
-    img.alt = item.alt;
-    img.className = "w-full h-full object-cover";
-    li.appendChild(img);
-    gallery.appendChild(li);
-  }
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.getElementById("header");
   const home = document.getElementById("home");
@@ -69,7 +42,6 @@ window.addEventListener("load", function () {
     loading.classList.add("hidden");
   }, 2000);
 
-  populateGallery();
   Reveal([...document.querySelectorAll(".hero-card")]);
   Reveal([...document.querySelectorAll(".methodology")]);
 });
